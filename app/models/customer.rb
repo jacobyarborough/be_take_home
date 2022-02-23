731451df4b96e50be0cbb25292325052df4111eb
Jacob Yarborough <jayarborough95@gmail.com>
2022-02-22 20:44:34 -0700

    Add customer model spec

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  has_many :customer_subscriptions
+  has_many :subscriptions, through: :customer_subscriptions
+
+  validates_presence_of :first_name, :last_name, :email, :address
 end
