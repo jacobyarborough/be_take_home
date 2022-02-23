0aab5b27c7788c96b01efdaf49116b5f7b47d52d
Jacob Yarborough <jayarborough95@gmail.com>
2022-02-22 20:36:10 -0700

    Add subscription model spec

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,4 @@
 class Subscription < ApplicationRecord
   belongs_to :tea
+  validates_presence_of :title, :price, :frequency, :tea_id
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:tea) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:price) }
+  it { should validate_presence_of(:frequency) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:tea_id) }
 end
