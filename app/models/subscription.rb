73a7d94593b337405daa4fcd1b47ccf93ac3df42
Jacob Yarborough <jayarborough95@gmail.com>
2022-02-22 20:49:26 -0700

    Update subscription model spec

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,6 @@
 class Subscription < ApplicationRecord
   belongs_to :tea
+  has_many :customer_subscriptions
+  has_many :customers, through: :customer_subscriptions
   validates_presence_of :title, :price, :frequency, :tea_id
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
   it { should belong_to(:tea) }
+  it { should have_many(:customer_subscriptions) }
+  it { should have_many(:customers).through(:customer_subscriptions) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:frequency) }
