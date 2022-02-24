dfe1df359198a482e219de0f49b0ee38d6bf6b20
Jacob Yarborough <jayarborough95@gmail.com>
2022-02-23 17:48:50 -0700

    Create subscription request spec

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::SubscriptionsController < ApplicationController 
   def index
-    customer = Customer.find(subscription_params[:customer_id])
-    render json: SubscriptionSerializer.new(customer.subscriptions)
+    if !subscription_params[:customer_id] || subscription_params[:customer_id] == "" || subscription_params[:customer_id] == " "
+      render json: { error: 'Customer is invalid or left blank', status: 400 }, status: 400 
+    elsif !Customer.find(subscription_params[:customer_id])
+      render json: { error: 'Customer is invalid or left blank', status: 400 }, status: 400 
+    else 
+      customer = Customer.find(subscription_params[:customer_id])
+      render json: SubscriptionSerializer.new(customer.subscriptions)
+    end
   end 
 
   private 
--- spec/requests/api/v1/subscriptions_spec.rb
+++ spec/requests/api/v1/subscriptions_spec.rb
@@ -1,25 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe 'Subscriptions', type: :request do 
-  describe 'GET /api/v1/subscriptions' do 
-    let(:teas) { create(:tea, 2) }
-    let(:subscription1) { create(:subscription, frequency: 'weekly', tea_id: teas.first.id) }
-    let(:subscription2) { create(:subscription, frequency: 'biweekly', tea_id: teas.last.id) }
-    let(:customer) { create(:customer) }
-    let(:customer_subscription1) { create(:customer_subscription, customer_id: customer.id, subscription_id: subscription1.id, status: 'active') }
-    let(:customer_subscription2) { create(:customer_subscription, customer_id: customer.id, subscription_id: subscription2.id, status: 'canceled') }
+  let!(:teas) { create_list(:tea, 2) }
+  let!(:subscription1) { create(:subscription, frequency: 'weekly', tea_id: teas.first.id) }
+  let!(:subscription2) { create(:subscription, frequency: 'biweekly', tea_id: teas.last.id) }
+  let!(:customer) { create(:customer) }
+  let!(:customer_subscription1) { create(:customer_subscription, customer_id: customer.id, subscription_id: subscription1.id, status: 'active') }
+  let!(:customer_subscription2) { create(:customer_subscription, customer_id: customer.id, subscription_id: subscription2.id, status: 'canceled') }
+
+  describe 'GET /api/v1/subscriptions' do
     let(:params) { {"customer_id": customer.id } }
-    before { get "/api/v1/subscriptions" ,params: params }
+    before { get "/api/v1/subscriptions", params: params }
 
     context 'when the request is valid' do 
       it 'returns all subscriptions for a customer' do 
         subs = JSON.parse(response.body, symbolize_names: :true)[:data]
-        binding.pry
         expect(subs).not_to be_empty
-        expect(subs).to be_a(Hash)
-        epect(subs[:type]).to eq('subscription')
-        expect(subs).to have_key(:title)
+        expect(subs).to be_an(Array)
+        expect(subs.count).to eq(2)
+        expect(subs[0][:type]).to eq('subscription')
+        expect(subs[0]).to have_key(:id)
+        expect(subs[0][:id]).to be_a(String)
+        expect(subs[0][:attributes]).to be_a(Hash)
+        expect(subs[0][:attributes]).to have_key(:title)
+        expect(subs[0][:attributes][:title]).to be_a(String)
+        expect(subs[0][:attributes]).to have_key(:price)
+        expect(subs[0][:attributes][:price]).to be_a(Float)
+        expect(subs[0][:attributes]).to have_key(:frequency)
+        expect(subs[0][:attributes][:frequency]).to be_a(String)
+        expect(subs[0][:attributes]).to have_key(:tea_id)
+        expect(subs[0][:attributes][:tea_id]).to be_an(Integer)
       end 
+
+      it 'returns status code 201' do 
+        expect(response).to be_successful 
+        expect(response).to have_http_status(200)
+      end 
+    end 
+
+    context 'the customer does not exist' do 
+      let(:params) { {"customer_id": 1000001}}
     end 
   end 
 end 
